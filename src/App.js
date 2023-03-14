31b55aaa26001afa60f3691e5aa63b87a684af97
Anant kumar <akuj1sep@gmail.com>
2023-03-15 05:06:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Data from "./quiz-data.js";
+import './App.css'
 
-function App() {
+const App = () => {
+
+  const [answers,setAnswers]=useState(new Array(Data.length).fill(-1));
+  const [score,setScore]=useState(null);
+
+  const handleOptionClicked=(questionNo,selectedOption)=>{
+          const newAns=[...answers];
+          newAns[questionNo]=selectedOption;
+          setAnswers(newAns);
+  }
+
+  const handleSubmit=()=>{
+    let totalScore=0;
+    for(let i=0;i<Data.length;i++)
+    {
+      if(answers[i]===Data[i].correctAnswerIndex)
+      {
+        totalScore++;
+      }
+    }
+    setScore(totalScore);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Data.map((item, questionNo) => {
+        return (
+          <div key={item.id} className="question">
+            {item.question}
+            <ul className="option-wrapper">
+              {item.options.map((option, optionNo) => {
+                return (<li className="options"
+                key={optionNo}
+                onClick={()=>handleOptionClicked(questionNo,optionNo)}>
+                  {option}
+                </li>);
+              })}
+            </ul>
+          </div>
+        );
+      })}
+
+      <button className="check-ans" onClick={handleSubmit}>Check Answers</button>
+      {
+        score!==null && 
+        ( 
+          <div className="result">
+            <h3 className="your-score">your Score : {score}/{Data.length}</h3>
+            <div >
+            {
+              Data.map((item,index)=>{
+                return(
+                  <div key={item.id}>
+                    <p className="result-ques">{item.question}</p>
+                    <p className="correct-ans">Correct Answer:   {item.options[item.correctAnswerIndex]}</p>
+                    <p className="your-ans">Your Answer:      {item.options[answers[index]]}</p>
+                  </div>
+                  
+                )
+              })
+            }
+            </div>
+          </div>
+        )}
     </div>
   );
-}
+};
 
 export default App;
+
+// import React from "react"
+// import './App.css';
+// import Data from "./quiz-data"
+// import Main from "./main";
+// import {nanoid} from "nanoid"
+
+// export default function App() {
+
+//   function options(index)
+//   {
+//    const item=Data.results[index];
+//    const ans={
+//      opt:[...item.incorrect_answers,item.correct_answer],
+//      isSelected:false,
+//      id:nanoid()
+//    }
+//      const size=ans.opt.length;
+//      let randomIndex=Math.floor(Math.random()*size)
+//      if(randomIndex!==size-1)
+//      {
+//          [ans.opt[3],ans.opt[randomIndex]]=[ans.opt[randomIndex],ans.opt[3]]
+//      }
+//    return(
+//              <div className="answers">
+//               {ans}
+//                { ans.opt.map(options=>{
+//                  return (<div className="options">{options}</div>)
+//              })}
+//              </div>
+//        )
+//   }
+
+//    function quesAns()
+//      {
+//         return( Data.results.map((item,index)=>{
+//              return (
+//              <div>
+//                  <div className="questions">{item.question}</div>
+//                 {options(index)}
+
+//              </div>)
+//            }))
+//      }
+//   return (
+//     <main>
+//       <Main
+//       quesAns={quesAns()}
+//       />
+//       <button className="check-answer"> Check Answers</button>
+//     </main>
+//   );
+// }
+
+
+
---
+++ src/quiz-data.js
@@ -0,0 +1,65 @@
+
+const Data= [
+      {
+        "id": 1,
+        "question": "What is the capital of France?",
+        "options": ["London", "Paris", "Berlin", "Madrid"],
+        "correctAnswerIndex": 1
+      },
+      {
+        "id": 2,
+        "question": "What is the largest planet in our solar system?",
+        "options": ["Saturn", "Mars", "Jupiter", "Venus"],
+        "correctAnswerIndex": 2
+      },
+      {
+        "id": 3,
+        "question": "What is the smallest country in the world by land area?",
+        "options": ["Monaco", "Vatican City", "Nauru", "San Marino"],
+        "correctAnswerIndex": 1
+      },
+      {
+        "id": 4,
+        "question": "Who invented the telephone?",
+        "options": ["Alexander Graham Bell", "Thomas Edison", "Nikola Tesla", "Albert Einstein"],
+        "correctAnswerIndex": 0
+      },
+      {
+        "id": 5,
+        "question": "What is the tallest mammal?",
+        "options": ["Elephant", "Giraffe", "Hippopotamus", "Rhinoceros"],
+        "correctAnswerIndex": 1
+      },
+      {
+        "id": 6,
+        "question": "What is the name of the world's largest desert?",
+        "options": ["Sahara", "Gobi", "Atacama", "Antarctica"],
+        "correctAnswerIndex": 0
+      },
+      {
+        "id": 7,
+        "question": "Which planet is known as the Red Planet?",
+        "options": ["Mars", "Jupiter", "Saturn", "Venus"],
+        "correctAnswerIndex": 0
+      },
+      {
+        "id": 8,
+        "question": "What is the chemical symbol for gold?",
+        "options": ["Au", "Ag", "Cu", "Fe"],
+        "correctAnswerIndex": 0
+      },
+      {
+        "id": 9,
+        "question": "What is the national animal of Australia?",
+        "options": ["Kangaroo", "Emu", "Koala", "Platypus"],
+        "correctAnswerIndex": 0
+      },
+      {
+        "id": 10,
+        "question": "Who directed the movie Titanic?",
+        "options": ["Steven Spielberg", "James Cameron", "Quentin Tarantino", "Martin Scorsese"],
+        "correctAnswerIndex": 1
+      }
+    ]
+  export default Data;
+  
